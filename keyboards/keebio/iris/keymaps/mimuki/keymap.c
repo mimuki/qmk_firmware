124197f24f4f8b843bcc0413c0c040610fbdfd2c
Mimuki <mimuki@protonmail.com>
2022-06-28 20:02:12 +0930

    quickly fixing stuff
    need to redo this to not suck later lol

--- keyboards/keebio/iris/keymaps/mimuki/keymap.c
+++ keyboards/keebio/iris/keymaps/mimuki/keymap.c
@@ -97,16 +97,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //       ├───────┼───────┼───────┼───────┼───────┼───────┤                       ├───────┼───────┼───────┼───────┼───────┼───────┤
   //       │  Tab  │   a   │   r   │   s   │   t   │   g   │                       │   m   │   n   │   e   │   i   │   o   │   '   │
   //       ├───────┼───────┼───────┼───────┼───────┼───────┼───────┐       ┌───────┼───────┼───────┼───────┼───────┼───────┼───────┤
-  //       │ Ctrl  │   z   │   x   │   c   │   d   │   v   │  Alt  │       │  Del  │   k   │   h   │   ,   │   .   │   /   │LAltEnt│
+  //       │ Gui  │   z   │   x   │   c   │   d   │   v   │  Alt  │       │  Del  │   k   │   h   │   ,   │   .   │   /   │    Ent│
   //       └───────┴───────┴───────┴───┬───┴───┬───┴───┬───┴───┬───┘       └───┬───┴───┬───┴───┬───┴───┬───┴───────┴───────┴───────┘
-  //                                   │  Gui  │Numbers│  Bksp │               │ Space │ Shift │ PrtSc │     
-  //                                   └───────┴───────┴───────┘               └───────┴───────┴───────┘        
+  //                                   │  LCtrl │Numbers│  Bksp │               │ Space │ Shift │ RAlt │     
+  //    TODO: unfuck this hotfix lol                               └───────┴───────┴───────┘               └───────┴───────┴───────┘        
 
         PB_1,    PB_2,    PB_3,    PB_4,    PB_5,    PB_6,                           PB_7,    PB_8,    PB_9,    PB_10,   PB_11,   PB_12,
         KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                           KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, DF(1),
         KC_TAB,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                           KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-        MOD_LCTL, KC_Z,   KC_X,    KC_C,    KC_D,   KC_V,  MOD_LALT,        KC_DEL, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, ENT_ALT,
-                                            MOD_LGUI, OSL(2),KC_BSPC,        SPC_LT3, SFT_CAP, PRTSCR
+        MOD_LGUI, KC_Z,   KC_X,    KC_C,    KC_D,   KC_V,  MOD_LALT,        KC_DEL, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+                                            MOD_LCTL, OSL(2),KC_BSPC,        SPC_LT3, KC_RSFT, MOD_RALT
   ),
 
   [_QWERTY] = LAYOUT(
